4fface37c7d1ef5d96a2cd06f69bebee115fd054
Tran Manh Hung <hungmanh.t94@gmail.com>
2018-11-13 22:32:40 +0100

    DU7 not done but im tired

--- 07-math_but_music/music.py
+++ 07-math_but_music/music.py
@@ -4,23 +4,118 @@ import struct as st
 import sys
 import wave as w
 import math
+import heapq
 
 AMPLITUDE = 20
 
 WTF = ['C', 'Cis', 'D', 'Es', 'E',
        'F', 'Fis', 'G', 'Gis', 'A', 'Bes', 'B']
 
-C0 = 16
+
+def getPeak(window):
+    amplitudes = np.abs(np.fft.rfft(window))
+    limit = np.mean(amplitudes) * AMPLITUDE
+    peaks = [amp if amp >= limit else 0 for amp in amplitudes]
+
+    maxPeaks = []
+    for i in range(3):
+        maxPeak = heapq.nlargest(3, peaks)
+        if maxPeak[i] == 0:
+            break
+        index = peaks.index(maxPeak[i])
+        maxPeaks.append(index)
+        try:
+            peaks[index-1] = 0
+        except IndexError:
+            pass
+        try:
+            peaks[index+1] = 0
+        except IndexError:
+            pass
+
+    return maxPeaks.sort()
 
 
-def pitch(freq):
+def pitch(freq, a4):
+    C0 = a4 * pow(2, -4.75)
     h = round(12 * math.log2(freq/C0))
     octave = h // 12
     n = h % 12
+
     return WTF[n] + str(octave)
 
 
-def main(fileName):
+def diff(freq):
+    h = round(12 * math.log2(freq/C0))
+
+    return round(1200 * math.log2(freq / pow(2, (h / 12)) * C0))
+
+
+def diffToString(freq):
+    dif = diff(freq)
+
+    if int(dif) > 0:
+        return " + {}".format(dif)
+    elif int(dif) == 0:
+        return ""
+    else:
+        return " - {}".format(str(dif)[1:])
+
+
+def pitchToString(freq, a4):
+
+    pitc = pitch(freq, a4)
+    parser = re.search(r'(\w+)(-)?(\d+)', pitc, re.IGNORECASE)
+
+    if (parser.group(3) != "0"):
+        if (parser.group(2) is not None):
+            res = "{},,{}".format(parser.group(
+                1), ("," * int(parser.group(3))))
+
+        elif int(parser.group(3)) < 3:
+            res = "{}{}".format(parser.group(
+                1), ("," * (2 - int(parser.group(3)))))
+
+        else:
+            res = "{}{}".format(parser.group(1)[:1].lower(
+            ) + parser.group(1)[1:], ("’" * (int(parser.group(3)) - 3)))
+
+    else:
+        res = "{},,".format(parser.group(1))
+
+    return res
+
+
+def toString(freq, a4):
+    return pitchToString(freq, a4) + diffToString(freq)
+
+
+def getResult(amplitude):
+    amplitude = round(amplitude, 2)
+    if amplitude > 0.1:
+        return amplitude, diffToString(amplitude), toString(amplitude), getPeak(amplitude)
+    return None
+
+
+def printResult(peaks, a4):
+    start = 0
+    end = 0
+    pitches = []
+    for time, peak in enumerate(peaks):
+        if time > 0:
+            if peak != peaks[time-1]:
+                print("{:.1f}-{:.1f} {}".format(start, end, " ".join(pitches)))
+                start = end
+                pitches = map(lambda x: toString(x, a4), peak)
+        else:
+            pitches = map(lambda x: toString(x, a4), peak)
+        end += 0.1
+
+    print("{:.1f}-{:.1f} {}".format(start,
+                                    end, " ".join(pitches)))
+
+
+def main(a4, fileName):
     reader = w.open(fileName, 'rb')
 
     rate = reader.getframerate()
@@ -39,6 +134,12 @@ def main(fileName):
 
     fmt = '%dh' % channels * rate
 
+    window = []
+    windowSize = rate / 10
+    filteredData = []
+    result = []
+
+    # read data
     while reader.tell() < reader.getnframes():
         rawdata = reader.readframes(rate)
         if len(rawdata) != reqLength:
@@ -46,36 +147,24 @@ def main(fileName):
 
         structData = st.unpack(fmt, rawdata)
 
-        filteredData = []
-
         for i in range(0, len(structData), 2):
             chunk = structData[i: i + 2]
             filteredData.append((chunk[0] + chunk[1]) / 2)
 
-        amplitudes = np.fft.rfft(filteredData)
-        averageAmplitude = sum(map(np.abs, amplitudes)) / len(amplitudes)
+    # process data
+    for value in filteredData:
+        window.append(value)
+        if len(window) != windowSize:
+            continue
 
-        amplitudes = enumerate(amplitudes)
+        result.append(getPeak(window))
+        window = []
 
-        filteredAmplitudes = filter(lambda amplitude: np.abs(
-            amplitude[1]) >= (averageAmplitude * AMPLITUDE), amplitudes)
-
-        filteredAmplitudes = map(lambda x: x[0], filteredAmplitudes)
-
-        for freq in filteredAmplitudes:
-            if minPeak is None or minPeak > freq:
-                minPeak = freq
-            if maxPeak is None or maxPeak < freq:
-                maxPeak = freq
-            # print(freq, np.abs(amplitude))
-
-    if minPeak is None or maxPeak is None:
-        print("no peaks")
-    else:
-        print("low = {}, high = {}".format(minPeak, maxPeak))
-
+    # agregate data ???????????????
+    result = filter(None, result)
+    printResult(result, float(a4))
     reader.close()
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2])
